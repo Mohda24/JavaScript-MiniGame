35da71c1376b86bda6c87abdb77c474f39eff7c7
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-03-14 01:08:00 +0100

    I add some of js code of my firstgame

---
+++ script.js
@@ -0,0 +1,8 @@
+// Staarting select selectors
+number=document.querySelector("header .number")
+myinput=document.getElementById("mynum")
+myfirstP=document.querySelector('.text p:first-child')
+score=document.querySelector('.text p:nth-child(2) span')
+hightscore=document.querySelector('.text p:last-child')
+
+
